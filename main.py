76209137f84fe5b2fa146f62b3d8186046669294
Sandro Vianna Lobão <svlobao@bol.com.br>
2023-07-30 18:26:10 -0300

    Haar cascade real-time face detection

--- main.py
+++ main.py
@@ -26,8 +26,12 @@ def accessMacbookCamera():
     cap = cv2.VideoCapture(0)
 
     while True:
-        ret, img = cap.read()
-        cv2.imshow("Frame", img)
+        ret, frame = cap.read()
+
+        detectFace(frame=frame)
+
+        cv2.imshow("Video", frame)
+
         if cv2.waitKey(1) & 0xFF == ord("q"):
             break
 
@@ -36,6 +40,22 @@ def accessMacbookCamera():
 
     return
 
+
+def detectFace(frame):
+    face_cascade = cv2.CascadeClassifier(
+        cv2.data.haarcascades + "haarcascade_frontalface_default.xml"
+    )
+
+    gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+
+    faces = face_cascade.detectMultiScale(
+        gray, scaleFactor=1.1, minNeighbors=5, minSize=(30, 30)
+    )
+
+    for x, y, w, h in faces:
+        cv2.rectangle(frame, (x, y), (x + w, y + h), (0, 255, 0), 2)
+
+    return
 
 
 if __name__ == "__main__":
